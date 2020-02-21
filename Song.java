bceaae7413737b8523caa2fb761b869ceb4a895b
NikhilTaneja98 <60517862+NikhilTaneja98@users.noreply.github.com>
2020-02-21 21:49:36 +0530

    File Creating Song Type Object

---
+++ Song.java
@@ -0,0 +1,13 @@
+class Song
+{
+    String Songname,Songpath,Posterpath;
+    
+
+    Song(String Songname,String Songpath,String Posterpath)
+    {
+        this.Songname=Songname;
+        this.Songpath=Songpath;
+        this.Posterpath=Posterpath;
+        
+    }
+}
